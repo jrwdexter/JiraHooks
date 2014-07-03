293cfe8fb32320d1f7d98445feaea66ff94eda86
Jonathan Dexter <jdexter@nerdery.com>
2014-07-03 14:04:10 -0500

    Updated the setup script to add jira-creds to .gitignore.

--- setup-git-hooks.sh
+++ setup-git-hooks.sh
@@ -10,6 +10,8 @@ ln -s "$DIR/git_hooks" "$DIR/.git/hooks"
 echo -e "\nThis script will setup git hook \033[0;32msymlinks\033[0m, as well as storing your JIRA \033[0;31musername\033[0m and \033[0;31mpassword\033[0m for use with commits."
 echo -e "Username:Password pair is base 64 encoded and stored in the current directory under \033[0;31mjira-creds\033[0m."
 echo -e "\n\n\033[0;34mNote:\033[0m A warning looking like 'WARNING: can't open config file' may appear."
+
+# Get username/password
 echo -e "\nPlease enter your JIRA \033[0;31musername\033[0m: "
 read USERNAME
 echo -e "\nPlease enter your JIRA \033[0;31mpassword\033[0m: "
@@ -17,6 +19,12 @@ read PASSWORD
 
 openssl enc -base64 <<< "$USERNAME:$PASSWORD" > "$DIR/jira-creds"
 
+# Add jira-creds to .gitignore
+if ! grep -q "jira-creds" "$DIR/.gitignore" ; then
+    printf "\n\n# Jira Credentials\njira-creds" >> "$DIR/.gitignore"
+fi
+
+# Test authentication from JIRA with new creds.
 echo -e "\nTesting JIRA credentials, please wait..."
 
 RESULT=$(curl -s -D- -I -X GET -H "Authorization: Basic $(cat $DIR/jira-creds)" -H "Content-Type: application/json" "https://issues.nerdery.com/rest/api/2/project") > /dev/null
